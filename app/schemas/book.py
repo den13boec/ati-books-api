6a2c72a7ee44028de592e18752b0513ab4bceb2a
den13boec <den-boris@list.ru>
2025-05-31 20:00:29 +0300

    Migrate to Pydantic v2 style

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -1,13 +1,14 @@
+from typing import Annotated
 from pydantic import BaseModel, Field
 
+
 class BookCreate(BaseModel):
-    title: str = Field(..., description="Название книги")
-    author: str = Field(..., description="Имя автора")
-    published_year: int = Field(..., description="Год публикации")
+    title: Annotated[str, Field(description="Название книги")]
+    author: Annotated[str, Field(description="Имя автора")]
+    published_year: Annotated[int, Field(description="Год публикации")]
+
 
 class Book(BookCreate):
-    id: int = Field(..., description="Уникальный идентификатор")
+    id: Annotated[int, Field(description="Уникальный идентификатор")]
 
-    model_config = {
-        "from_attributes": True
-    }
+    model_config = {"from_attributes": True}
