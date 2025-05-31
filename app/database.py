e167e2c990c7a575ff9955521dcf647dd100643c
den13boec <den-boris@list.ru>
2025-06-01 00:52:21 +0300

    Add get_db dependency for session management

--- app/database.py
+++ app/database.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from app.config import settings
 
 engine = create_engine(settings.db_url)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
