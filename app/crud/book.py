a780c64d9c4c899fad19fd732bf4f41e9b1ea2e2
den13boec <den-boris@list.ru>
2025-05-31 20:01:24 +0300

    Add CRUD operations for Book

---
+++ app/crud/book.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from app.models.book import Book
+from app.schemas.book import BookCreate
+
+
+def create_book(db: Session, book: BookCreate) -> Book:
+    db_book = Book(**book.model_dump())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+def get_books(db: Session, author: str | None = None):
+    query = db.query(Book)
+    if author:
+        query = query.filter(Book.author.ilike(author))
+    return query.all()
+
+
+def get_book_by_id(db: Session, book_id: int):
+    return db.query(Book).filter(Book.id == book_id).first()
