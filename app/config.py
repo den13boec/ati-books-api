fcfb40d46359b2ff0f648251adc1565f9b9b7512
den13boec <den-boris@list.ru>
2025-06-04 17:55:06 +0300

    Fix DB connection to support custom ports

--- app/config.py
+++ app/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
     POSTGRES_HOST: str
-    POSTGRES_PORT: int
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
@@ -14,7 +13,7 @@ class Settings(BaseSettings):
     def db_url(self) -> str:
         return (
             f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
-            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+            f"@{self.POSTGRES_HOST}:5432/{self.POSTGRES_DB}"
         )
 
 
